2ed499daa803bdf26882cffb573ff4a037a8b91e
Dixon <59415488+radixon@users.noreply.github.com>
2022-04-26 15:36:29 -0400

    Update 002WeeklyChallenge.cpp

--- 002WeeklyChallenge.cpp
+++ 002WeeklyChallenge.cpp
@@ -9,21 +9,21 @@
 
 	#include <iostream>
 	
-	void fizzbuzz(int n)
+	void fizzbuzz(int n, int x = 3, int y = 5)
 	{
 		// Start with the the multiples of both 3 and 5 so the program prints and exits if a multiple of both
 		// Check for zero as zero is not a multiple of 3 or 5
-		if(n % (3*5) == 0 && n != 0)
+		if(n % (x*y) == 0 && n != 0)
 			std::cout << "FizzBuzz" << '\t';
 		
 		// Check for the highest multiple for readability.  Descending in order makes the logic easier to follow
 		// Check for zero as zero is not a multiple of 3 or 5
-		else if(n % 5 == 0 && n != 0)
+		else if(n % y == 0 && n != 0)
 			std::cout << "Buzz" << '\t';
 			
 		// Check for the lowest multiple for readability.  Descending in order makes the logic easier to follow
 		// Check for zero as zero is not a multiple of 3 or 5
-		else if(n % 3 == 0 && n != 0)
+		else if(n % x == 0 && n != 0)
 			std::cout << "Fizz" << '\t';
 			
 		// Print input if the input is not a multiple of 3 or 5
